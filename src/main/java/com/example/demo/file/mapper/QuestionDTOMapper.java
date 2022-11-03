1aa28970d524193c9040dcf7114f3a2b6edff1e1
Atul Kumar <atul.ak@gmail.com>
2022-11-03 17:06:14 +0530

    Added mission attribute option

--- src/main/java/com/example/demo/file/mapper/QuestionDTOMapper.java
+++ src/main/java/com/example/demo/file/mapper/QuestionDTOMapper.java
@@ -1,15 +1,23 @@
 package com.example.demo.file.mapper;
 
+import com.example.demo.file.dto.OptionDTO;
 import com.example.demo.file.dto.QuestionDTO;
 import com.example.demo.file.dto.SubTopicDTO;
 import com.example.demo.file.model.Quiz;
 
-import java.util.Arrays;
-import java.util.UUID;
+import java.util.*;
 
 public class QuestionDTOMapper {
 
     public static QuestionDTO getQuestionDTO(Quiz quiz) {
+        List<OptionDTO> optionDTOList = new ArrayList<>();
+        quiz.getOptions().forEach((k, v) -> {
+            optionDTOList.add(OptionDTO.builder()
+                    .option(k)
+                    .value(String.valueOf(v))
+                    .optionType("text").build());
+        });
+
         return QuestionDTO.builder()
                 .id(UUID.randomUUID().toString())
                 .serialNumber(Integer.valueOf(quiz.getQuestionSerialNo()))
@@ -31,6 +39,8 @@ public class QuestionDTOMapper {
                 .qualityGrade(quiz.getQualityGrade())
                 .isGuided("Yes") //check the template for value
                 .primaryConcept(quiz.getConceptTips())
+                .options(optionDTOList)
                 .build();
     }
+
 }
